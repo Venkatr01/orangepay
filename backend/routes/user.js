3c153f60007724a9e110f0c6bc20e5400c48b780
Venkatr01 <vraju4865@gmail.com>
2024-01-28 15:28:50 +0530

    Add signin logic

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -13,7 +13,12 @@ const signupBody = zod.object({
     password:zod.string()
 })
 
-router.use('/signup', async (req,res)=>{
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string
+})
+
+router.post('/signup', async (req,res)=>{
     const {success} = signupBody.safeParse(req.body)
     if(!success){
         return res.status(411).json({
@@ -41,7 +46,35 @@ router.use('/signup', async (req,res)=>{
 
 })
 
+router.post('./signin',async (req,res) =>{
+    const {success} = signinBody.safeParse(req.body)
+    if(!success){
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
 
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if(user){
+        const token = jwt.sign({
+            userId: user._id
+        },JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+
+})
 
 
 
