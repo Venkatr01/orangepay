54a5b844eeb7022bc0e551758faf8ea04755038b
Venkatr01 <vraju4865@gmail.com>
2024-01-28 14:27:53 +0530

    Add signup logic

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,53 @@
 const express = require('express');
+const zod = require('Zod');
+const { User } = require('../db');
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../config')
 
 const router = express.Router();
+
+const signupBody = zod.object({
+    username: zod.string().email(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password:zod.string()
+})
+
+router.use('/signup', async (req,res)=>{
+    const {success} = signupBody.safeParse(req.body)
+    if(!success){
+        return res.status(411).json({
+            message: "Email already taken/Incorrect inputs"
+        })
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.username,
+        
+    })
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET)
+
+    res.json({
+        message:"User created successfully",
+        token: token
+    })
+
+})
+
+
+
+
+
+
+
+
 
 module.exports = {
     router
