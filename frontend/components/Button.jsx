23c788091b6f837c9afd877ff71ef8e1e0d75019
Venkatr01 <vraju4865@gmail.com>
2024-01-31 22:23:37 +0530

    Add button component

---
+++ frontend/components/Button.jsx
@@ -0,0 +1,7 @@
+export function Button(){
+    return (
+        <>
+            <input type="button" value="Sign Up" className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-12 border border-blue-700 rounded w-full" />        
+        </>
+    )
+}
