4a9bc38f47b8b8768f45c096c7fa3a42a641ed3a
venku <venku@Vbuntu.myguest.virtualbox.org>
2024-01-31 22:30:10 +0530

    Remove Heading Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import { Dashboard } from "../pages/dashboard";
+import { SendMoney } from "../pages/send";
+import { Signin } from "../pages/signin";
+import { Signup } from "../pages/Signup";
+import { Heading } from "../components/heading";
+
+
 function App() {
 
   return (
     <>
-      Hi there
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+      <Heading ></Heading>
     </>
   )
 }
