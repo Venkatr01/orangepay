058118f48a83bd3f035f7c698e7097fca259ed56
venku <venku@Vbuntu.myguest.virtualbox.org>
2024-02-03 12:14:39 +0530

    Branch commit check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Homepage  /> } />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
