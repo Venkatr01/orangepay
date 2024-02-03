ed4c097de25c6b651863229fee835452018e7097
venku <venku@Vbuntu.myguest.virtualbox.org>
2024-02-03 09:21:29 +0530

    Modify BE route

--- frontend/components/Balance.jsx
+++ frontend/components/Balance.jsx
@@ -7,7 +7,7 @@ export const Balance = ({ value }) => {
 
 
     useEffect(()=>{
-        axios.get("http://localhost:3000/api/v1/account/balance",{
+        axios.get("https://wallet-backed.onrender.com/api/v1/account/balance",{
             headers: {
                 Authorization: "Bearer " + localStorage.getItem('token')
             }
